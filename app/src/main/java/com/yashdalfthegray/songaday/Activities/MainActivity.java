87fbadd3f73d621a33ff14fb7770db73abb7b48b
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-08-02 23:31:25 -0500

    added a fab and wired up onClick for settings button and fab

--- app/src/main/java/com/yashdalfthegray/songaday/Activities/MainActivity.java
+++ app/src/main/java/com/yashdalfthegray/songaday/Activities/MainActivity.java
@@ -1,9 +1,10 @@
 package com.yashdalfthegray.songaday.Activities;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -49,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Log.d("MainActivity", "Settings button pressed");
             return true;
         }
 
--- app/src/main/java/com/yashdalfthegray/songaday/Fragments/SongListFragment.java
+++ app/src/main/java/com/yashdalfthegray/songaday/Fragments/SongListFragment.java
@@ -2,7 +2,9 @@ package com.yashdalfthegray.songaday.Fragments;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,9 @@ import com.yashdalfthegray.songaday.R;
  */
 public class SongListFragment extends Fragment {
 
+    View inflatedView;
+
+    private FloatingActionButton addFab;
 
     public SongListFragment() {
         // Required empty public constructor
@@ -24,8 +29,24 @@ public class SongListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_song_list, container, false);
+        this.inflatedView = inflater.inflate(R.layout.fragment_song_list, container, false);
+
+        addFab = (FloatingActionButton)inflatedView.findViewById(R.id.add_fab);
+        addFab.setOnClickListener(onClickListener);
+
+        return this.inflatedView;
     }
 
-
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.add_fab:
+                    Log.d("SongListFragment", "Add button pressed");
+                    break;
+                default:
+                    Log.w("SongListFragment", "Don't know what happened there!");
+            }
+        }
+    };
 }
