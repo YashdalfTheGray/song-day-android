f232008af682e0bfdaed163704bdaf7283d2c719
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-08-05 15:12:32 -0500

    song now implements parcelable so that it can be stuffed in a god damn bundle

--- app/src/main/java/com/yashdalfthegray/songaday/Models/Song.java
+++ app/src/main/java/com/yashdalfthegray/songaday/Models/Song.java
@@ -1,16 +1,28 @@
 package com.yashdalfthegray.songaday.Models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.Date;
 
 /**
  * Created by yash on 8/3/15.
  */
-public class Song {
+public class Song implements Parcelable{
     String artist, title, link, genre, date;
     Date datePosted;
 
     public Song () {
     }
+
+    public Song(Parcel in) {
+        this.artist = in.readString();
+        this.title = in.readString();
+        this.link = in.readString();
+        this.genre = in.readString();
+        this.date = in.readString();
+    }
+
 
     public String getArtist () {
         return artist;
@@ -37,4 +49,32 @@ public class Song {
     public String toString() {
         return getTitle() + " by " + getArtist() + ", " + getGenre() + ", " + getLink() + ". Posted at " + getDate();
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.artist);
+        dest.writeString(this.title);
+        dest.writeString(this.link);
+        dest.writeString(this.genre);
+        dest.writeString(this.date);
+    }
+
+    public static final Creator<Song> CREATOR
+            = new Creator<Song>() {
+
+        @Override
+        public Song createFromParcel(Parcel source) {
+            return new Song(source);
+        }
+
+        @Override
+        public Song[] newArray(int size) {
+            return new Song[size];
+        }
+    };
 }
