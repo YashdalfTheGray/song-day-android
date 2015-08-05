2f09ab59fc05be7fd445e976737ce5662748aee3
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-08-04 22:32:55 -0500

    added a song adapter and a super simple list row layout

---
+++ app/src/main/java/com/yashdalfthegray/songaday/Adapters/SongAdapter.java
@@ -0,0 +1,52 @@
+package com.yashdalfthegray.songaday.Adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.yashdalfthegray.songaday.Models.Song;
+import com.yashdalfthegray.songaday.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Yash Kulshrestha on 8/4/2015.
+ */
+public class SongAdapter extends RecyclerView.Adapter<SongAdapter.ViewHolder> {
+    private ArrayList<Song> mSongList;
+
+    public SongAdapter(ArrayList<Song> songs) {
+        mSongList = songs;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d("SongAdapter", "viewType - " + viewType);
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.song_list_row, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        TextView titleView = (TextView)holder.view.getRootView().findViewById(R.id.title_view);
+        titleView.setText(mSongList.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mSongList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public View view;
+
+        public ViewHolder(View v) {
+            super(v);
+            view = v;
+        }
+    }
+}
