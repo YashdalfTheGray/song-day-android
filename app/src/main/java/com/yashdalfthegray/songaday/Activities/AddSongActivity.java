36bb25e01e12924c4c7473a00a286418f8861585
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-08-17 16:34:46 -0500

    changed up the add song activity code to be compliant with appcompat

--- app/src/main/java/com/yashdalfthegray/songaday/Activities/AddSongActivity.java
+++ app/src/main/java/com/yashdalfthegray/songaday/Activities/AddSongActivity.java
@@ -2,6 +2,7 @@ package com.yashdalfthegray.songaday.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -9,31 +10,24 @@ import com.yashdalfthegray.songaday.R;
 
 public class AddSongActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_song);
+
+        mToolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_song, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
