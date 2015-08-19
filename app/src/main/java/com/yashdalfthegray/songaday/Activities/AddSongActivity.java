75de265bd4f1ad9eba92e38fc3886061213e5a65
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-08-18 20:14:24 -0500

    wired up the add/save button and changing title based on operation

--- app/src/main/java/com/yashdalfthegray/songaday/Activities/AddSongActivity.java
+++ app/src/main/java/com/yashdalfthegray/songaday/Activities/AddSongActivity.java
@@ -1,21 +1,37 @@
 package com.yashdalfthegray.songaday.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.yashdalfthegray.songaday.R;
 
 public class AddSongActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private Button actionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_song);
+
+        String activityMode = getIntent().getStringExtra(MainActivity.SONG_ACTIVITY_MODE);
+
+        actionButton = (Button)findViewById(R.id.song_action_button);
+
+        if(activityMode.equals(MainActivity.ADD_MODE)) {
+            setTitle("Add a song");
+            actionButton.setText("Add");
+        }
+        else if (activityMode.equals(MainActivity.EDIT_MODE)) {
+            setTitle("Edit song");
+            actionButton.setText("Save");
+        }
 
         mToolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
